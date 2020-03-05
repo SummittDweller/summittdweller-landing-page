c14531c10fe18b404311b7397542df944585de16
SummittDweller <summitt.dweller@gmail.com>
2020-03-05 15:43:59 -0600

    Moving to static-like landing page

--- push-update.sh
+++ push-update.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-cd ~/Projects/summittdweller-landing-page
-perl -i.bak -lpe 'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
-docker image build -t summittdweller-landing-update .
-# docker login
+
+# Get the current date and time
+dt=$(date '+%Y-%m-%d %H:%M')
+# Substitute current date/time into config.toml's buildDate parameter.  Works in OSX, may need alteration in Linux?
+sed -i'.bak' -e "s/buildDate = .*/buildDate = '${dt}'/" config.toml
+
+# Make sure we are using the current Git branch
+current=`git symbolic-ref --short -q HEAD`
+git checkout ${current}
+
+# Compile the site before copying to the new image
+hugo --ignoreCache --ignoreVendor --minify --debug --verbose
+echo "Hugo compilation is complete."
+
+# Build a new Docker image
+echo "Starting docker image build..."
+docker image build -f push-update-Dockerfile --no-cache -t summittdweller-landing-update .
+echo "...docker image build is complete."
+
+# Tag the new image and push it to Docker Hub
+docker login
 docker tag summittdweller-landing-update summittdweller/summittdweller-landing:latest
 docker push summittdweller/summittdweller-landing:latest
